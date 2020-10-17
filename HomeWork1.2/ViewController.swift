92578fde5202e7d2dca83b53d3d36c89414c47ab
Nurzhan <a.nurzhanq@mail.ru>
2020-10-18 01:17:37 +0600

    Initial Commit

---
+++ HomeWork1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork1.2
+//
+//  Created by Abilkaiyr Nurzhan on 10/18/20.
+//  Copyright © 2020 Abilkaiyr Nurzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
